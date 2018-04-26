aaff3027d4008707a58920227291e73b34826fc9
Wei Zheng <ivan.zheng1201@gmail.com>
2018-04-25 21:49:49 -0400

    bug fixed 2

--- code/data/cityscapesmask.py
+++ code/data/cityscapesmask.py
@@ -11,7 +11,7 @@ import torch.utils.data as data
 
 class cityscapesmask(srdata.SRData):
     def __init__(self, args, train=True):
-        super(cityscapes, self).__init__(args, train)
+        super(cityscapesmask, self).__init__(args, train)
         self.repeat = args.test_every // (args.n_train // args.batch_size)
 
     def _scan(self):
